83c6d8541e90371b5a447972a5a73499b2b1ed53
narendragolla <narendragolla44@gmail.com>
2023-10-06 23:58:16 +0530

    App

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import  ReactDOM,{createRoot} from "react-dom";
-const root=ReactDOM.createRoot(document.getElementById("root"));
-const heading=(
-    <h1>Hello world</h1>
-)
-root.render(heading)
