924e15ced5cd604f0fd153de65ca41a2457f4981
narendragolla <narendragolla44@gmail.com>
2023-10-13 20:26:16 +0530

    filterData

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,46 @@
 import {restaurantList} from './contants';
 import RestaurantCard from './RestaurantCard'
+import { useState } from 'react';
+
+function filterData(searchText, restaurants) {
+   
+    const filterRestaurant = restaurants.filter((restaurant) =>
+    restaurant?.info?.name?.toLowerCase()?.includes(searchText.toLowerCase())
+    );
+    return filterRestaurant;
+  }
+
+
 const Body=()=>{
+    const[searchText, setSearchText]=useState("");
+    const[restaurants,setRestaurants]=useState(restaurantList);
+    const handleChange=(event)=>{
+        setSearchText(event.target.value);
+    }
+    const handleSubmit=(event)=>{
+        event.prevent.default();
+        setSearchText(searchText);
+        
+    }
     return (
+        <>
+        
+        <div className="search-container">
+            <input type="text" className='search-input' placeholder='search' value={searchText} onChange={handleChange} />
+            <button onClick={
+                ()=>{
+                const data=filterData(searchText,restaurants);
+                setRestaurants(data)
+                }
+                }>Search</button>
+        </div>
+     
         <div className='restaurant-list'>
-            {restaurantList.map((restaturant)=>{
-                return <RestaurantCard {...restaturant.info} key={restaturant.info.id}/>
+            {restaurants.map((restaturant)=>{
+                return <RestaurantCard {...restaturant.info} key={restaturant?.info?.id}/>
             })}
         </div>
+        </>
     )
 }
 export default Body;
