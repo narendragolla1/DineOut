5af097e90b617fc6b1efdad59a204bfd88f044c7
narendragolla <narendragolla44@gmail.com>
2023-10-13 20:26:43 +0530

    Added new fields

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,11 +1,12 @@
-export default function RestaurantCard({name,cuisines,cloudinaryImageId,LastMileTravelString}){
+export default function RestaurantCard({name,cuisines,cloudinaryImageId,areaName,costForTwo}){
     return(
         <div className="card">
             <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
           cloudinaryImageId}/>
           <h2>{name}</h2>
-          <h3>{cuisines.join(',')}</h3>
-          <h4>{LastMileTravelString}</h4>
+          <h4 >{cuisines.join(',')}</h4>
+          <h4>{areaName}</h4>
+          <h5>Cost for Two: {costForTwo}</h5>
         </div>
     );
 }
