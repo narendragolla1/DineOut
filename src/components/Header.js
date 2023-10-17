2d8d4e5fad0c83b9698b7da345ded7aac5c722ec
narendragolla <narendragolla44@gmail.com>
2023-10-18 02:07:53 +0530

    added link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,34 @@
-import {IMG_CDN_URL} from '../contants';
+import { useState } from 'react';
+import {LOGO_IMG_CDN_URL} from './contants';
+import {Link} from "react-router-dom";
 const Title = () => (
     <a href="/">
       <img
         className="logo"
         alt="logo"
-        src={IMG_CDN_URL} />
+        src={LOGO_IMG_CDN_URL} />
     </a>
   );
+  const LoggedInUser=()=>{
+    return false;
+  }
   const Header = () => {
+    const[isLoggedIN,setIsLoggedIN]=useState(false);
     return (
       <div className="header">
         <Title />
         <div className="nav-items">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <Link to="/"><li>Home</li></Link>
+            <Link to="about"><li>About</li></Link>
+            <Link to="contact"><li>Contact</li></Link>
             <li>Cart</li>
           </ul>
         </div>
+        {isLoggedIN?(
+          <button onClick={()=>setIsLoggedIN(false)}>LogOut</button>):
+            (<button onClick={()=>setIsLoggedIN(true)}>Login</button>)
+          }
       </div>
     );
   };
