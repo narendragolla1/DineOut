259e537ca8b4b814102699c6c53d54ea1fadf47e
narendragolla <narendragolla44@gmail.com>
2023-10-18 02:06:58 +0530

    router

--- src/App.js
+++ src/App.js
@@ -3,19 +3,45 @@ import  ReactDOM,{createRoot} from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import {createBrowserRouter, RouterProvider,Outlet} from "react-router-dom";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
 const AppLayout=()=>{
     return(
         <>
         <Header/>
-        <Body/>
+        <Outlet/>
         <Footer/>
         </>
     )
-}
+};
+const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: "about", 
+          element: <About/>,
 
-
-
-
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+        },
+        {
+          path: "",
+          element: <Body />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+        {
+          path:"restaurant/:resId",
+          element:<RestaurantMenu/>
+        }
+    
+      ],
+    },
+  ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
